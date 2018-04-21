10327f9cd77aa45034d99e564adfe6a35e1728ad
Brienna Herold <brienna.herold@gmail.com>
2018-04-20 22:43:49 -0400

    Add route to home page

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set a route to home page
+app.get('/', function(req, res) {
+	res.send('It works');
+});
+
 // Serve app on port 3000
-app.listen(3000);
-console.log('Server started on port 3000');
+app.listen(5000);
+console.log('Server started on port 5000');
+
 module.exports = app;
